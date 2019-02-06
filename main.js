8a27d226479d51d1fc5f3c9e4c3ceeedee3be5b2
Kurt <kurtmilla1994@gmail.com>
2019-02-06 11:31:44 -0500

    updated bottom container

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ function timeToday() {
     return container.innerHTML ='<strong>Todays Date: </strong> ' 
     + time.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'});
 }
-timeToday();
+timeToday();*/ // Uneeded script for time at this moment
 function myDropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
     document.getElementById("menu").classList.toggle("animate");
-}*/ // Uneeded script for time at this moment
+} // Dropdown menu
